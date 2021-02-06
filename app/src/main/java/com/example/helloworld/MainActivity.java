ecc1b6968d09b546da86c777b84d55f6f1b01f53
codingbaby <vsmysee@gmail.com>
2021-02-06 15:25:17 +0800

    add

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class MainActivity extends AppCompatActivity {
 
     final List<String> blogData = new ArrayList<>();
@@ -28,7 +29,24 @@ public class MainActivity extends AppCompatActivity {
 
     static {
         blogSite.put("http://www.yinwang.org", "li.list-group-item > a");
+        blogSite.put("https://afoo.me/posts.html", "header.title > div > h2 > a");
+        blogSite.put("https://tech.meituan.com/", "h2.post-title > a");
+        blogSite.put("http://www.ruanyifeng.com/blog/archives.html", "#alpha-inner > div.module-categories > div.module-content > ul.module-list > li > a");
+        blogSite.put("https://insights.thoughtworks.cn/tag/featured/", "a[rel=bookmark]");
+        blogSite.put("https://insights.thoughtworks.cn/", "a[rel=bookmark]");
+
+
         articleSite.put("https://www.36kr.com", "a.article-item-title");
+        articleSite.put("https://www.ifanr.com/", "a.js-title-transform");
+        articleSite.put("https://toutiao.io/posts/hot/7", "h3.title > a");
+        articleSite.put("http://www.kejilie.com", "h3.am_list_title > a");
+        articleSite.put("https://www.tmtpost.com/lists/latest_list_new", "li.part_post > div.info > a > h3");
+        articleSite.put("https://segmentfault.com/news/", "h4.news__item-title");
+        articleSite.put("https://www.tuicool.com/ah/20/", "a[style=display: block]");
+        articleSite.put("https://www.jdon.com/", "div.important > div.info > a");
+        articleSite.put("https://www.geeksforgeeks.org/", "div.content > div.head > a");
+
+
     }
 
 
@@ -43,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
 
         final ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+
+        Button refreshBtn = findViewById(R.id.article_refresh);
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                data.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
 
         Button blogBtn = findViewById(R.id.blog_button);
         blogBtn.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +95,13 @@ public class MainActivity extends AppCompatActivity {
                                         blogData.add(item.text());
                                         data.add(item.text());
                                     }
+
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            adapter.notifyDataSetChanged();
+                                        }
+                                    });
                                 }
 
                             } catch (Exception e) {
@@ -75,12 +109,6 @@ public class MainActivity extends AppCompatActivity {
                             }
 
 
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    adapter.notifyDataSetChanged();
-                                }
-                            });
 
                         }
 
@@ -133,18 +161,18 @@ public class MainActivity extends AppCompatActivity {
                                         articleData.add(item.text());
                                         data.add(item.text());
                                     }
+
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            adapter.notifyDataSetChanged();
+                                        }
+                                    });
                                 }
 
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    adapter.notifyDataSetChanged();
-                                }
-                            });
 
 
                         }
