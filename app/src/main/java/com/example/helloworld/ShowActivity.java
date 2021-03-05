a90a6518a57219d85b520d245ac7a47d6953b5c9
codingbaby <vsmysee@gmail.com>
2021-03-05 21:17:08 +0800

    add

--- app/src/main/java/com/example/helloworld/ShowActivity.java
+++ app/src/main/java/com/example/helloworld/ShowActivity.java
@@ -20,7 +20,7 @@ public class ShowActivity extends AppCompatActivity {
         setContentView(R.layout.blog);
 
         TextView textView = findViewById(R.id.back);
-        textView.setText("返回");
+        textView.setText("<<返回");
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
